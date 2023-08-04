c7c542190a84e8a6ba34507adb7358f19e72d735
Manu krishnan <106807168+Manu1305@users.noreply.github.com>
2023-08-04 14:40:48 +0530

    build error solved

--- src/const/config.js
+++ src/const/config.js
@@ -1,4 +1,4 @@
-export const apiURL = 'https://hitechmart.onrender.com'
+export const apiURL = 'https://demo-hitechmart.onrender.com'
 
 // razorpay API Key
 export const RAZ_API_KEY_TEST = "rzp_test_EvyMmsf90H0SWx"
