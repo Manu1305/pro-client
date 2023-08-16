82725411f559253343fee64f8f8421ab94b5a775
Manu krishnan <106807168+Manu1305@users.noreply.github.com>
2023-08-16 15:21:24 +0530

    complete table

--- src/Component/App.js
+++ src/Component/App.js
@@ -23,7 +23,6 @@ import Editprofile from "./Dashboard/Sellersettings/sellerprofilesettings/EditPr
 import AddProduct from "./Navbar/Profile/SellerDashboard/ProductSec/Addproduct";
 import Withdraw from "./Dashboard/Withdraw/Withdraw";
 import SellerSettingsPage from "./Dashboard/Sellersettings/SellerSettings";
-
 import Thankyou from "./Navbar/Login/Buyer&SellerRegister/SellerRegistration/Thankyou";
 import AboutUs from "./Header/Home/AboutSection/About";
 import cards from "./Header/Home/NewArrival/NewArrival/Cards";
@@ -92,7 +91,9 @@ const App = () => {
               />
             }
           />
-          <Route path="/dashboard" element={<SellerDashboard />} />
+          <Route path="/dashboard" element={<SellerDashboard />} >
+            
+          </Route>
           {/* <Route
             path="/cart"
             element={
--- src/Component/Error Handling/Alert.jsx
+++ src/Component/Error Handling/Alert.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
-export const Alert = ({message}) => {
-  console.log("Alert")
-  const notify = () => toast(message);
-  useEffect(() => {
-    notify()
-  },[])
+// export const Alert = ({message}) => {
+//   console.log("Alert")
+//   const notify = () => toast(message);
+//   useEffect(() => {
+//     notify()
+//   },[])
 
-  return (
-    <div>
-      {/* <button onClick={notify}>Notify!</button> */}
-      <ToastContainer />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {/* <button onClick={notify}>Notify!</button> */}
+//       <ToastContainer />
+//     </div>
+//   );
+// };
