db2284f5468c4ee84010dbb225229ca2feeeb470
Manu krishnan <106807168+Manu1305@users.noreply.github.com>
2023-08-23 17:33:53 +0530

    apiurl

--- src/Component/Shopingsection/Shopping.jsx
+++ src/Component/Shopingsection/Shopping.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+ import React, { useEffect } from "react";
 import styless from "./Shopping.module.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
