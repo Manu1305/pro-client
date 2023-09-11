60cb722fbadfec7bff036936d5d701e737dc57cb
Akshay Neje <akshayneje2823@gmail.com>
2023-09-11 12:38:34 +0530

    addded eye icon

--- src/Component/Shopingsection/Shopping.jsx
+++ src/Component/Shopingsection/Shopping.jsx
@@ -23,6 +23,8 @@ const Shopping = () => {
   const [data, setData] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  
+
 
   const handleCategoryChange = (categor) => {
     if (categor === "all") {
@@ -47,7 +49,7 @@ const Shopping = () => {
         console.log(res.data);
 
         dispatch(addProduct(res.data));
-        const filByStaus = res.data.filter((prd) => prd.status === true);
+        const filByStaus = res.data.filter((prd) => prd.status === true)
         setData(filByStaus);
       })
       .catch((error) => {
@@ -60,10 +62,8 @@ const Shopping = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    data.length !== 0 && getAllProducts();
+    getAllProducts();
   }, []);
-
-
 
   const sellingPrices = data.map((item) => item.sellingPrice);
   const highestPrice = Math.max(...sellingPrices);
@@ -89,74 +89,69 @@ const Shopping = () => {
     .map((data) => {
       return (
         <div key={data.id} className="col-lg-3 col-md-4 mb-5">
-          <div className="d-flex flex-column">
-            <div className={styless.container}>
-              <Link
-                style={{
-                  cursor: "pointer",
-                  position: "relative",
-                  display: "inline-block",
-                }}
-                to={`/ViewDetails/${data._id}`}
-              >
-                <div style={{ position: "relative" }}>
-                  <img
-                    src={data.productDetails[0].images[0]}
-                    style={{ height: 320, width: 305, objectFit: "fill" }}
-                    alt=""
-                  />
-                  <div className={styless.like}>
-                    <div
-                      style={{
-                        position: "absolute",
-                        bottom: 0,
-                        right: 0,
-                        margin: "5px",
-                      }}
-                    >
-                      <PiHeartLight
-                        className={styless.like_button}
-                        style={{
-                          height: "30px",
-                          width: "30px",
-                          fontWeight: "50px",
-                        }}
-                        onClick={() => {
-                          console.log("Wish list");
-                        }}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
+        <div className="d-flex flex-column">
+        <div className={styless.container}>
+  <Link style={{ cursor: "pointer", position: "relative", display: "inline-block" }} to={`/ViewDetails/${data._id}`}>
+    <div style={{ position: "relative" }}>
+      <img
+        src={data.productDetails[0].images[0]}
+        style={{ height: 320, width: 305, objectFit: "fill" }}
+        alt=""
+      />
+       <div className={styless.like}>
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          margin: "5px",
+        }}
+      >
+        <PiHeartLight
+        className={styless.like_button}
+          style={{
+           
+            height:'30px',
+            width:'30px',
+            fontWeight: "50px",
+          }}
+          onClick={() => {
+            console.log("Wish list");
+          }}
+        />
+      </div>
+      </div>
+    </div>
+  </Link>
+</div>
 
-            {/* Description */}
-            <div className="card-body">
-              <div
-                className="cart-title m-1"
-                style={{ textTransform: "uppercase", fontFamily: "fantasy" }}
-              >
-                {data.brand}
-              </div>
-              {user && user.email ? (
-                <div className="m-2 d-flex justify-content-between">
-                  <div className="mb-1 me-1 mx-4" style={{ fontSize: "bold" }}>
-                    &#8377; {data.sellingPrice}{" "}
-                  </div>
-                </div>
-              ) : (
-                <div className="m-2" style={{ fontWeight: "30px" }}>
-                  {data.description}
-                </div>
-              )}
+          {/* Description */}
+          <div className="card-body">
+            <div
+              className="cart-title m-1"
+              style={{ textTransform: "uppercase", fontFamily: "fantasy" }}
+            >
+              {data.brand}
             </div>
+            {user && user.email ? (
+              <div className="m-2 d-flex justify-content-between">
+                <div className="mb-1 me-1 mx-4" style={{ fontSize: "bold" }}>
+                  &#8377; {data.sellingPrice}{" "}
+                </div>
+              </div>
+            ) : (
+              <div className="m-2" style={{ fontWeight: "30px" }}>
+                {data.description}
+              </div>
+            )}
           </div>
         </div>
+      </div>
+      
       );
     });
   const pageCount = Math.ceil(filteredProducts.length / usersPerpage);
-
+  
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
@@ -188,7 +183,10 @@ const Shopping = () => {
               <div className="row p-3">
                 <div className="col-lg-3">
                   <div className="card mb-5">
-                    <div className="card-body mb-7 shadow-xl">
+                    <div
+                     
+                      className="card-body mb-7 shadow-xl"
+                    >
                       <div style={{ color: "black", fontWeight: "bolder" }}>
                         PRODUCT CATEGORIES
                       </div>
@@ -291,17 +289,24 @@ const Shopping = () => {
                   >
                     {displayUsers}
 
-                    <div></div>
-                    {filteredProducts && filteredProducts.length !== 0 && (
-                      <ReactPaginate
-                        className={styless.pagination}
-                        previousLabel={"<-prev"}
-                        nextLabel={" next->"}
-                        pageCount={pageCount}
-                        onPageChange={changePage}
-                        containerClassName={"pagination"}
-                      />
-                    )}
+                <div>
+
+                </div>
+                {filteredProducts && filteredProducts.length !== 0 && (
+  <ReactPaginate
+    className={styless.pagination}
+    previousLabel={"<-prev"}
+    nextLabel={" next->"}
+    pageCount={pageCount}
+    onPageChange={changePage}
+    containerClassName={"pagination"}
+    
+  />
+)}
+
+                
+
+                   
                   </div>
                   <hr />
                 </div>
@@ -310,7 +315,7 @@ const Shopping = () => {
           </div>
         )}
       </div>
-      <div style={{ overflow: "hidden" }}>
+      <div style={{ overflow:"hidden" }}>
         <Footer />
       </div>
     </>
