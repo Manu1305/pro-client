da2b430180b6b5562283932ca819bb10c2cb03a0
Amarjeet <amarjeet.kumar@ajiledone.com>
2023-09-16 09:54:20 +0530

    empty

--- src/Component/Navbar/Franchise/WholesaleStore.jsx
+++ src/Component/Navbar/Franchise/WholesaleStore.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import idea from "./Franchise.module.css";
-import { AiOutlineStar } from "react-icons/ai";
-import { Link } from "react-router-dom";
+
 
 export const WholesaleStore = () => {
   return (
